90f21ba5c1994d444a29b83a1f2d7c7085f2710e
Cory Goad <thebrickgoat@gmail.com>
2024-02-28 17:07:11 -0500

    tailwind/vite init

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    // Use prefix tw- to avoid conflicts with other Dawn styles
+    // prefix: 'tw-',
+    content: [
+        "./**/*.{js,json,liquid}",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [
+    ],
+}
