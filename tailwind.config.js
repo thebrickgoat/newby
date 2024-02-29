ea132b696e585dcdd2025a66e42a175270b2fba9
Cory Goad <thebrickgoat@gmail.com>
2024-02-28 19:35:29 -0500

    prefix updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
     // Use prefix tw- to avoid conflicts with other Dawn styles
-    // prefix: 'tw-',
+    prefix: 'tw-',
     content: [
         "./**/*.{js,json,liquid}",
     ],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                'heading': 'var(--font-heading-family)'
+            },
+        },
     },
     plugins: [
     ],
